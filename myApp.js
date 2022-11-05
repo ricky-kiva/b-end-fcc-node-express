86043a486434c94e2df26e8a82ed1cf319206105
vento <r.ckyslash@gmail.com>
2022-11-05 20:13:11 +0700

    fix PATH on sendFile '/'

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ let app = express();
 console.log("Hello World")
 
 app.get('/', function (req, res) { // app.METHOD(PATH, HANDLER). Handler is a function with (req, res) param
-    res.sendFile(__dirname + "views/index.html") // HANDLER.res.sendFile will send file from given path as it's param
+    res.sendFile(__dirname + "/views/index.html") // HANDLER.res.sendFile will send file from given path as it's param
 })
 
 app.get('/', function (req, res) { // app.METHOD(PATH, HANDLER). Handler is a function with (req, res) param
