df00d27bda86ed2a7ca21c57ef8a103c5fa03d86
vento <r.ckyslash@gmail.com>
2022-11-05 19:30:45 +0700

    first get method to send string

--- myApp.js
+++ myApp.js
@@ -3,37 +3,8 @@ let app = express();
 
 console.log("Hello World")
 
+app.get('/', function (req, res) { // app.METHOD(PATH, HANDLER). Handler is a function with (req, res) param
+    res.send('Hello Express') // sending the string to '/' directory
+  })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
