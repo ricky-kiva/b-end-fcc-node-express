379f6b0056fc3675a577e5b8341ee0c427867f78
vento <r.ckyslash@gmail.com>
2022-11-05 19:43:31 +0700

    send index.html as response to get '/'

--- myApp.js
+++ myApp.js
@@ -4,7 +4,11 @@ let app = express();
 console.log("Hello World")
 
 app.get('/', function (req, res) { // app.METHOD(PATH, HANDLER). Handler is a function with (req, res) param
+    res.sendFile(__dirname + "views/index.html") // HANDLER.res.sendFile will send file from given path as it's param
+})
+
+app.get('/', function (req, res) { // app.METHOD(PATH, HANDLER). Handler is a function with (req, res) param
     res.send('Hello Express') // sending the string to '/' directory
-  })
+})
 
 module.exports = app;
